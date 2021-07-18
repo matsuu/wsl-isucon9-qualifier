1d622c03e872809f9611d5a400fbdf46039dcccc
MATSUU Takuto <matsuu@gmail.com>
2021-07-18 09:11:07 +0900

    use git shallow clone

--- scripts/03-ansible.sh
+++ scripts/03-ansible.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y ansible curl git 
 
-git clone https://github.com/matsuu/ansible-isucon9-qualify.git
+git clone --depth=1 https://github.com/matsuu/ansible-isucon9-qualify.git
 cd ansible-isucon9-qualify
 ansible-playbook --connection=local -i webapp, standalone.yml
 cd ..
